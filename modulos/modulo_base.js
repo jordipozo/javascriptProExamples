423f7e704a3887bcb0824a679a7255cab6cf42d9
jpozoc <jpozoc@gmail.com>
2020-05-04 13:48:49 +0200

    Actualización 04052020

---
+++ modulos/modulo_base.js
@@ -0,0 +1,6 @@
+//import { name } from './modulo_dos.mjs';
+//console.log(name);
+
+import def, {name,edad} from './modulo_dos.mjs'
+
+console.log(def,name,edad);
---
+++ modulos/modulo_dos.mjs
@@ -0,0 +1,5 @@
+export const name='Jordi'; //variable a exportar-- export nos indica esto
+export const edad=25;
+let valorPorDefecto = 250;
+
+export default valorPorDefecto;
